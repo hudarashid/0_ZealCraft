be5d9bb725bd741b2c5858c24917e85e860c5020
Rohan DSouza <67008593+dsouza28@users.noreply.github.com>
2022-03-15 16:17:25 +0000

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -2,18 +2,18 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema(
     {
-        modelId: { type: Number, required: true },
+        //modelId: { type: Number, required: true },
         productName: { type: String, required: true },
         productDescription: { type: String, required: true },
         images: { type: Image },
-        unitOfMeasure: { type: String },
-        quantityOnHand: { type: Number },
-        weight: { type: Number },
+        unitOfMeasure: { type: String, required: true },
+        quantityOnHand: { type: Number, required: true },
+        weight: { type: Number, required: true },
         currentPrice: { type: Number, required: true },
         discountedPrice: { type: Number },
-        isFeatured: { type: String, required: true },
+        isFeatured: { type: Boolean, required: true },
         productStatus: { type: String, required: true },
-        categoryId: { type: Number, required: true },
+        productCategoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'ProductCategory', required: true },
     },
     {
         timestamps: true,
