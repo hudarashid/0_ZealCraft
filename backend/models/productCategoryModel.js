96aebbe95fea6064dd7bf4832d0ffa5153562888
Rohan DSouza <67008593+dsouza28@users.noreply.github.com>
2022-03-15 16:14:00 +0000

    Update productCategoryModel.js

--- backend/models/productCategoryModel.js
+++ backend/models/productCategoryModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const productCategorySchema = new mongoose.Schema(
     {
-        categoryId: { type: Number, required: true },
+        //categoryId: { type: Number, required: true },
         categoryName: { type: String, required: true },
         categoryDescription: { type: String, required: true },
         categoryStatus: { type: String, required: true },       
