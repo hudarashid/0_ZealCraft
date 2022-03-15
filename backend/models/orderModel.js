7fc9d542d92f82d4a9b22b4346e185b995f948bc
Rohan DSouza <67008593+dsouza28@users.noreply.github.com>
2022-03-15 16:12:57 +0000

    Update orderModel.js
    Changes to the FK

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 
 const orderSchema = new mongoose.Schema(
     {
-        orderId: { type: Number, required: true },
+        //orderId: { type: Number, required: true },
         orderDate: { type: Date, required: true },
         orderStatus: { type: String, required: true },
         deliveryInstructions: { type: String, required: true },
         totalPrice: { type: Number, required: true },
-        customerId: { type: Number, required: true },
-        userId: { type: Number, required: true },
+        customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     },
     {
         timestamps: true
