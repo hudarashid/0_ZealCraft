246310a7f590a2314f32147ab1d0d37c1b61cd84
Rohan DSouza <67008593+dsouza28@users.noreply.github.com>
2022-04-03 18:22:07 +0100

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,28 @@
 import express from 'express';
-import path from 'path';
+import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import userRouter from './routes/userRoutes.js';
-import orderRouter from './routes/orderRoutes.js';
-import uploadRouter from './routes/uploadRoutes.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import userRouter from './routes/userRouter.js';
+import categoryRouter from './routes/categoryRouter.js';
+import productRouter from './routes/productRouter.js';
+import uploadRouter from './routes/uploadRouter.js';
+// import storeRouter from './routes/storeRouter.js';
+import seedRouter from './routes/seedRouter.js';
+import path from 'path';
+
+const app = express();
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    //useCreateIndex: true,
+    useNewUrlParser: true,
+    //useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('connected to db');
   })
@@ -19,31 +30,50 @@ mongoose
     console.log(err.message);
   });
 
-const app = express();
-
+//const app = express(); //Function that returns express app object.
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   })
+// );
 
-app.get('/api/keys/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
-});
+// app.use('/images', express.static(path.dirname('images')));
+// app.use('/images', express.static(path.dirname('images')));
 
-app.use('/api/upload', uploadRouter);
+// app.use('/api/seed', seedRouter);
+// app.get('/api/users', (req, res) => {
+//   res.send(data.users);
+// });
+// app.use('/api/users', userRouter);
+app.use('/api/images', uploadRouter);
+// app.use(express.static('/public/'));
+
 app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
+app.use('/api/ur', userRouter);
+app.use('/api/cr', categoryRouter);
+app.use('/api/pr', productRouter);
+// app.use('/api/sr', storeRouter);
+// app.get('/api/products', (req, res) => {
+//   res.send(data.products);
+// });
+
+// app.get('/api/categories', (req, res) => {
+//   res.send(data.categories);
+// });
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
+//test
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
