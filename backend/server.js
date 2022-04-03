bf87036145c1f537ce980746b30260a252f5c7ff
Rohan DSouza <67008593+dsouza28@users.noreply.github.com>
2022-04-03 18:39:31 +0100

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import userRouter from './routes/userRouter.js';
+import categoryRouter from './routes/categoryRouter.js';
+import productRouter from './routes/productRouter.js';
+import uploadRouter from './routes/uploadRouter.js';
+// import storeRouter from './routes/storeRouter.js';
+import seedRouter from './routes/seedRouter.js';
+import path from 'path';
+
+const app = express();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    //useCreateIndex: true,
+    useNewUrlParser: true,
+    //useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+//const app = express(); //Function that returns express app object.
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   })
+// );
+
+// app.use('/images', express.static(path.dirname('images')));
+// app.use('/images', express.static(path.dirname('images')));
+
+// app.use('/api/seed', seedRouter);
+// app.get('/api/users', (req, res) => {
+//   res.send(data.users);
+// });
+// app.use('/api/users', userRouter);
+app.use('/api/images', uploadRouter);
+// app.use(express.static('/public/'));
+
+app.use('/api/seed', seedRouter);
+app.use('/api/ur', userRouter);
+app.use('/api/cr', categoryRouter);
+app.use('/api/pr', productRouter);
+// app.use('/api/sr', storeRouter);
+// app.get('/api/products', (req, res) => {
+//   res.send(data.products);
+// });
+
+// app.get('/api/categories', (req, res) => {
+//   res.send(data.categories);
+// });
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+//test
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
---
+++ backend/utils.js
@@ -0,0 +1,72 @@
+import jwt from 'jsonwebtoken';
+//import multer from 'multer';
+//import path from 'path';
+
+export const generateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      address: user.address,
+      city: user.city,
+      postalCode: user.postalCode,
+      country: user.country,
+      phone: user.phone,
+      isAdmin: user.isAdmin,
+      isUser: user.isUser,
+      isCustomer: user.isCustomer,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+    }
+  );
+};
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: 'Invalid Token' });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: 'No Token' });
+  }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin === true) {
+    next();
+  } else {
+    res.status(401).send({ message: 'Invalid Admin Token' });
+  }
+};
+
+export const isUser = (req, res, next) => {
+  if (req.user && req.user.isUser === true) {
+    next();
+  } else {
+    res.status(401).send({ message: 'Invalid Admin Token' });
+  }
+};
+
+//Multer config
+//module.exports = multer({
+  //storage: multer.diskStorage({}),
+  //fileFilter: (req, file, cb) => {
+    //let ext = path.extname(file.originalname);  
+    //if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+      //cb(new Error("File type is not supported"), false);
+      //return;
+    //}
+    //cb(null, true);
+  //},
+//});
